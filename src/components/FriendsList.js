2df41e74fb428463e7195bfc727f82fc34ea6bdd
Casey Harding <casey.r.harding@gmail.com>
2022-01-18 12:46:21 -0500

    Through task 7.

--- src/components/FriendsList.js
+++ src/components/FriendsList.js
@@ -2,6 +2,7 @@ import React from 'react'
 // 👉 1- FriendsList renders several Friend components, import Friend
 
 export default function FriendsList(props) {
+  console.log(props);
   // 👉 2- What data does FriendsList need to do its job? Use destructuring
   return (
     <div className='list-friends container'>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 // Importing React libs from node_modules folder
 import React from 'react'
+// export default React;
+// default export
+
 import { render } from 'react-dom'
+// export const render = () => {}
+// named export
 
 // Importing the styles - the bundler does some magic!
 import './styles.less'
